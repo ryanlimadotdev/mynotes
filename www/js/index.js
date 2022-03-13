8064cb9495261a15934181ce2eadf78920a1b5c9
Ryan <ryanlimati@protonmail.com>
2022-03-13 12:47:24 -0300

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Get the elements
+const boards = document.getElementById("boards");
+
+const card = {};
+
+
+
+const addNote = function(element) {
+	element.content.appendChild('<div style="color: red; font-size:20px;">Hello</div>');
+};
+
+
+
+
+console.log(boards);
